1cae3db3e4c65017fbdc441d3515884db1880887
Odudu Francis <leefrancis2020@gmail.com>
2022-04-07 17:17:17 -0700

    my coomit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio .h>                                                                                                                   
+
+/**                                                                                                                                   
+
+ *main - Entry point                                                                                                                  
+
+ *My hello, printf                                                                                                                    
+
+ *Return: return 1 after program execution                                                                                            
+
+ */                                                                                                                                   
+
+int main(void)                                                                                                                        
+
+{                                                                                                                                     
+
+printf("with proper grammer, but the outcome is a piece of art, \n");
+return (0);
+}
